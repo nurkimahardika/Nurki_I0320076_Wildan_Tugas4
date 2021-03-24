0e2d45b66ba0b11b13b38957b7e1f6d3a1c1a4dc
Nurki Putra Mahardika <nurput28@gmail.com>
2021-03-25 01:14:53 +0700

    Soal 3

---
+++ soal 3.py
@@ -0,0 +1,10 @@
+#berat_max = 50 lbs, 1 lbs = 0,45 kg
+berat_max = 50*0.45
+print('Berat maksimum dalam kg adalah ',berat_max, 'kg')
+soalA = 110
+soalB = 49
+
+hasil1 = soalA < berat_max
+hasil2 = soalB < berat_max
+print('Jika berat maksimum adalah', berat_max,'kg, maka dengan berat lebih 110 kg, hasilnya menjadi ', hasil1,)
+print('Jika berat maksimum adalah', berat_max,'kg, maka dengan berat 49 kg, hasilnya menjadi ', hasil2,)
